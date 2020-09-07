2dd68aac0df56870977a72850fe645344c5cd6d3
tracht <marija.trachtenberg@gmail.com>
2020-09-07 13:45:41 +0100

    Creates actions, constants, reducer, and store skeleton

---
+++ src/js/actions/index.js
@@ -0,0 +1,14 @@
+export function addArticle(payload) {
+  return {
+    type: "ADD_ARTICLE", 
+    payload
+  }
+}
+
+
+// NOTE!
+// You can notice that the type property is a string. 
+// Strings are prone to typos and duplicates and 
+// for this reason it's better to declare actions as constants. 
+// So we will create a new folder for them (constants)
+// And we will store the constants in a file called action-types.js
---
+++ src/js/reducers/index.js
@@ -0,0 +1,30 @@
+import { ADD_ARTICLE } from '../constants/action-types';
+
+const initialState = {
+  articles: []
+};
+
+function rootReucer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+   return Object.assign({}, state, {
+     articles: state.articles.concat(action.payload)
+   });
+  }
+  return state;
+};
+
+export default rootReducer;
+
+
+// NOTE!
+// we must use code in lines 9-10 because
+// we need to create a new state
+// the old state cannot be modified, due to redux rules of immutability
+// as such, we must return a new JS object
+
+// NOTE 2: 
+// The approach above ensures that the intial state is untouched
+// And the resulting state is just a copy of the initial state
+// The two key points to avoid mutations in redux: 
+// 1) use concat, slice, or the spread operator for arrays
+// 2) use Object.assign or object spreads for objects 

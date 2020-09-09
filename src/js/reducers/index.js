5a316aa9a12cf1e0131bbf7da3dd49119abc6519
tracht <marija.trachtenberg@gmail.com>
2020-09-09 12:15:10 +0100

    Fixes bugs

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,30 +1,17 @@
-// import { ADD_ARTICLE } from '../constants/action-types';
-import { ADD_ARTICLE } from '../actions/index';
-import { initialState } from './initialState';
-import { combineReducers } from 'redux';
+import { ADD_ARTICLE } from "../constants/action-types";
 
-// JavaScript 
-export const RootState = {
-  data: DataState
+const initialState = {
+  articles: []
 };
 
-export const dataReducer = (state = initialState, action) => {
-  switch (action.type) {
-
-    case action.ADD_ARTICLE:
-      return Object.assign({}, state, {
-        articles: state.articles.concat(action.payload)
-      });
-  
-    default: 
-      return state;  
-
+function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    state.articles.push(action.payload);
   }
-};
+  return state;
+}
 
-export const rootReducer = combineReducers({
-  data: dataReducer
-});
+export default rootReducer;
 
 // NOTE!
 // we must use code in lines 9-10 because we need to create a new state
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,10 +1,10 @@
 import { createStore, applyMiddleware } from "redux";
-import rootReducer from '../reducers/index';
-import { forbiddenWordsMiddleware } from '../middleware';
+import rootReducer from "../reducers/index";
+import { forbiddenWordsMiddleware } from "../middleware";
 
 const store = createStore(
   rootReducer,
-  applyMIddleWare(forbiddenWordsMiddleware)
-  );
+  applyMiddleware(forbiddenWordsMiddleware)
+);
 
-export default store; 
+export default store;
